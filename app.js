b4e19f70eb862e960ce3a65db0c0fa1ba7fb44b8
Jessé Toledo <h4ck3r.batera@gmail.com>
2021-08-21 23:42:00 -0300

    view engine

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.send("<html><body>Home</body></html>");
+});
+
+app.get('/technology', function(req, res){
+    res.render("section/technology");
+});
+
 app.listen(3000, function(){
-    console.log('teste express');
+    console.log('Init Express');
 });
